48fc1f012d716776beea30ddfa6de1c7b5364baf
Len Diaz <diazmarielen@gmail.com>
2025-04-05 20:59:01 +0800

    add custom css to tailwind.config.ts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{tsx,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero': "url('../src/assets/images/background.jpeg')",
+      },
+      colors: {
+        "deep-teal": "#0F282D",
+        "lime-green": "#D1FF3B",
+        "light-sky-blue": "#7AF1FF",
+        "purple": "#544AE5",
+      },
+      fontFamily: {
+        sans: ["ES Rebond Grotesque", "sans-serif"],
+      },
+      screens: {
+        "square-1080": {
+          raw: "(min-width: 1080px) and (min-height: 1080px)"
+        },
+        "vertical-1080x1920": {
+          raw: "(min-width: 1080px) and (min-height: 1920px)",
+        },
+      },
+    },
   },
   plugins: [],
 };
